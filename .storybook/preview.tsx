82975031f70efa2a5e1fe872bfd15b8cf51dd13d
sjmitch-git <sjmitch20@outlook.com>
2025-11-04 18:50:36 +0000

    Updates SEO

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import type { Preview } from "@storybook/nextjs-vite";
 import { withThemeByClassName } from "@storybook/addon-themes";
 import "../src/styles/index.css";
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
 
 const preview: Preview = {
   parameters: {
@@ -22,7 +29,12 @@ const preview: Preview = {
       },
       defaultTheme: "light",
     }),
-    (Story) => {
+    (Story, context) => {
+      useEffect(() => {
+        window.gtag("config", "G-JKVR4XLTMW", {
+          page_path: window.location.pathname + window.location.search,
+        });
+      }, [context.id]);
       return (
         <div className="preview-decorator bg-slate-50 dark:bg-slate-700 dark:text-light p-4">
           <Story />
